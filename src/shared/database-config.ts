1862ca37eb051fd52da13c5e23767b37d6f361e6
danielscarlazzetta <daniel.scarlazzetta@gmail.com>
2024-06-10 00:07:22 -0400

    create a entity user

--- src/shared/database-config.ts
+++ src/shared/database-config.ts
@@ -2,6 +2,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Auth } from 'src/auth/entities/auth.entity';
 import { Product } from 'src/product/entities/product.entity';
 import { Tag } from 'src/tags/entities/tag.entity';
+import { User } from 'src/user/entities/user.entity';
 
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'mysql',
@@ -10,6 +11,6 @@ export const databaseConfig: TypeOrmModuleOptions = {
   username: 'root',
   password: 'passmace123123',
   database: 'mace',
-  entities: [Auth, Product, Tag],
+  entities: [Auth, Product, Tag, User],
   synchronize: true,
 };
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,33 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+    
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ length: 50 })
+    nombre: string;
+
+    @Column({ length: 100 })
+    apePaterno: string;
+
+    @Column({ length: 100 })
+    apeMaterno: string;
+
+    @Column({ length: 100 })
+    email: string;
+
+    @Column({ length: 100 })
+    telefono: string;
+
+    @Column({ length: 100 })
+    calle: string;
+
+    @Column({ length: 100 })
+    departamento: string;
+
+    @Column({ length: 100 })
+    numero: string;
+    
+}
